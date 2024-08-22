129aa96ade997d4873db4f762a0ea9a9b4f16b83
Kuo-Yen Huang <kuoyen.huang@booking.com>
2024-08-22 18:50:47 +0200

    finish authentication

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require("./config/key");
+require("./models/user");
 require("./services/passport");
+
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,// the time will expire, minisecond unit.
+        keys: [keys.cookieKey]
+    })
+)
+
+app.use(passport.initialize());
+app.use(passport.session());
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 3000;
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,4 +8,13 @@ module.exports = (app) => {
   );
 
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout(); // passport auto attached the res and req for us, which clean the cookies for logout
+    res.send(req.user);
+  })
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+
+  })
 };
